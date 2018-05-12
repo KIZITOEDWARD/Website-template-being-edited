e0eb13547b369e4477fb57bb592becf126efe1d2
Sethb <kate.seth@icloud.com>
2018-05-12 14:12:51 -0400

    update to counter

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -5,7 +5,7 @@
  * License : MIT
  * Version : 1.3.2
  * Author : Vincent Loy <vincent.loy1@gmail.com>
- * Contributors : 
+ * Contributors :
  *  - Justin Beasley <JustinB@harvest.org>
  *  - Nathan Smith <NathanS@harvest.org>
  */
@@ -115,7 +115,7 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
+                year: 2018,
                 month: 6,
                 day: 28,
                 hours: 0,
